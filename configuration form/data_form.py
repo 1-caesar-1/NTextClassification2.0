8096e9dfdc6ad99365923404959fe5f6bad910f4
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-11-16 18:46:12 +0200

    add optin to run config file after making it

--- configuration form/data_form.py
+++ configuration form/data_form.py
@@ -3,6 +3,8 @@ from flaskwebgui import FlaskUI  # get the FlaskUI class
 from flask import render_template
 from write_file import data_parsing
 
+from os.path import dirname, abspath, exists
+
 app = Flask(__name__)
 ui = FlaskUI(app)
 ui.fullscreen = True
@@ -18,6 +20,15 @@ def index():
 @app.route("/data", methods=["POST"])
 def get_data():
     data_parsing(request)
+    return render_template("runFile.html")
+
+
+@app.route("/run")
+def run_file():
+    from main import main
+
+    parent_dir = dirname(dirname(abspath(__file__))) + "/configs"
+    main.main(parent_dir)
     return render_template("index.html")
 
 
