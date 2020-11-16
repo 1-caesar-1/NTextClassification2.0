46e923b1610e05f2c92f4ea96f356c1521c2c8b1
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-11-16 14:24:00 +0200

    add post data parsing

--- configuration form/data_form.py
+++ configuration form/data_form.py
@@ -5,8 +5,26 @@ from flask import render_template
 app = Flask(__name__)
 ui = FlaskUI(app)  # feed the parameters
 
+classifiers = ["svc", "rf", "lr", "mnb"]
 
+selection_type = {
+    "chi2": "chi2",
+    "mir": "mutual_info_regression",
+    "mic": "mutual_info_classif",
+    "fc": "f_classif",
+    "rfecv": "RFECV",
+    "sfm": "SelectFromModel",
+}
 # do your logic as usual in Flask
+data = {
+    "transformers": [],
+    "preprocessing": [],
+    "language": "",
+    "features_selection": [],
+    "measurements": [],
+    "classifiers": [],
+    "classification_technique": "TrainTest()",
+}
 
 
 @app.route("/")
@@ -16,7 +34,14 @@ def index():
 
 @app.route("/data", methods=["POST"])
 def get_data():
-    print(request.get_data())
+
+    for key in request.form:
+        if key in classifiers:
+            data["classifiers"].append(key)
+        if key in selection_type:
+            data["features_selection"].append(selection_type[key])
+
+    print(data)
     return render_template("index.html")
 
 
