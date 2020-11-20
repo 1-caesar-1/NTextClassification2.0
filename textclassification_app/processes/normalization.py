f566e266dc57e25e1c0a92621d3d512f42f6c530
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2020-11-20 13:35:20 +0200

    add functions

--- textclassification_app/processes/normalization.py
+++ textclassification_app/processes/normalization.py
@@ -6,9 +6,12 @@ import string
 import json
 from pathlib import Path
 import autocorrect
-
+from nltk.corpus import stopwords
 from autocorrect import Speller
 from nltk import WordNetLemmatizer, SnowballStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+from gensim.parsing.preprocessing import remove_stopwords as rs
 
 
 def normalize(experiment: Experiment):
@@ -64,13 +67,50 @@ def Repeated_characters(text: str):
 
 
 def Stemming(text: str):
-    pass
+    stemmer = SnowballStemmer("english")
+    text_lines = text.split("\n")
+    stemmed_lines = []
+    for line in text_lines:
+        line_words = line.split(" ")
+        stemmed_line = []
+        for word in line_words:
+            tokinized_word = word_tokenize(word)
+            stemmed_word = []
+            for part in tokinized_word:
+                stemmed_word.append(stemmer.stem(part))
+            stemmed_line.append("".join(stemmed_word))
+        stemmed_lines.append(" ".join(stemmed_line))
+    return "\n".join(stemmed_lines)
 
 
 def Lemmatizing(text: str):
-    pass
+    text_lines = text.split("\n")
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_lines = []
+    for line in text_lines:
+        line_words = line.split(" ")
+        lemmatized_line = []
+        for word in line_words:
+            tokinized_word = word_tokenize(word)
+            lemmatized_word = []
+            for part in tokinized_word:
+                lemmatized_word.append(lemmatizer.lemmatize(part))
+            lemmatized_line.append("".join(lemmatized_word))
+        lemmatized_lines.append(" ".join(lemmatized_line))
+    return "\n".join(lemmatized_lines)
 
 
-def stopwords(text: str):
-    pass
+def remove_stopwords(text: str):
+    return rs(text)
 
+
+def apostrophe_removal(text):
+    return text.replace("'", "").replace("'", "").replace("’", "")
+
+
+def acronyms_removal(text):
+    acr = set(re.findall(r"\w\.\w\.\w", text) + re.findall(r"\w+\"\w", text))
+    for acr in acr:
+        text = re.sub(acr, acr.replace(".", ""), text)
+        text = re.sub(acr, acr.replace('"', ""), text)
+    return text
