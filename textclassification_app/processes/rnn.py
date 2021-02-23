1f5323eba3a2b4c7e2ffbe6ad4831ae915ccf28a
mickeygoldmeier <45402517+mickeygoldmeier@users.noreply.github.com>
2021-02-23 15:04:57 +0200

    add results to rnn model

--- textclassification_app/processes/rnn.py
+++ textclassification_app/processes/rnn.py
@@ -9,15 +9,24 @@ from textclassification_app.classes.Experiment import Experiment
 
 def run_rnn(experiment: Experiment, cv=True):
     k_fold = KFold()
+    # for the convenience of reading
     X = experiment.documents
     y = experiment.labels
+
+    # initialize the result dict
+    result = dict()
+    for measure in experiment.measurements:
+        result[measure] = dict()
+        for model in experiment.classifiers:
+            result[measure][type(model).__name__] = list()
+
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.33, random_state=42
     )
 
     if cv:
         for train, test in k_fold.split(X, y):
-            for i in range(20):
+            for i in range(experiment.classification_technique.iteration):
                 model = get_model(X[train])
                 history = model.fit(
                     np.array(X[train]),
@@ -26,9 +35,12 @@ def run_rnn(experiment: Experiment, cv=True):
                     validation_data=(np.array(X[test]), y[test]),
                     validation_steps=10,
                 )
+                result["accuracy"]["RNNClassifier"] += history.history["val_accuracy"]
                 test_loss, test_acc = model.evaluate(np.array(X[test]), y[test])
                 print("Test Loss: {}".format(test_loss))
                 print("Test Accuracy: {}".format(test_acc))
+        experiment.classification_results = result
+        experiment.general_data["num_of_features"] = 0
     else:
         model = get_model(X_train)
         history = model.fit(
@@ -38,10 +50,12 @@ def run_rnn(experiment: Experiment, cv=True):
             validation_data=(np.array(X_test), y_test),
             validation_steps=10,
         )
-
+        result["accuracy"]["RNNClassifier"] += history.history["val_accuracy"]
         test_loss, test_acc = model.evaluate(np.array(X_test), y_test)
         print("Test Loss: {}".format(test_loss))
         print("Test Accuracy: {}".format(test_acc))
+    experiment.classification_results = result
+    experiment.general_data["num_of_features"] = 0
 
 
 def get_model(X):
